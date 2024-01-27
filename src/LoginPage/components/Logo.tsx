1316e4fa93a6ad864160fbb650d9691e1a30df21
Edgard <edgardbasiul@gmail.com>
2024-01-27 13:22:33 +0200

    deleted test

--- src/LoginPage/components/Logo.tsx
+++ src/LoginPage/components/Logo.tsx
@@ -3,7 +3,6 @@ const Logo = () => {
     <div className="font-yellowtail text-sky-500 font-bold text-center">
       <p className="text-4xl">Welcome to your</p>
       <span className="text-7xl">Task World !</span>
-      <p>HI</p>
     </div>
   );
 };
