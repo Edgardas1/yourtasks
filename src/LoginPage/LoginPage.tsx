c478e6a670fd0a7962fd169d6034f2a48800739e
Edgard <edgardbasiul@gmail.com>
2024-02-01 17:34:04 +0200

    adjusted screen for small devices

--- src/LoginPage/LoginPage.tsx
+++ src/LoginPage/LoginPage.tsx
@@ -3,7 +3,7 @@ import Logo from "./components/Logo";
 
 const LoginPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-slate-100 h-dvh">
+    <div className="flex flex-col justify-center items-center bg-slate-100 h-screen">
       <Logo />
       <SignInForm />
     </div>
