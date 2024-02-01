74e895ef1d7070a9fec87efd3ee925e44dbc562a
Edgard <edgardbasiul@gmail.com>
2024-02-01 07:22:00 +0200

    adjusted logo

--- src/LoginPage/components/Logo.tsx
+++ src/LoginPage/components/Logo.tsx
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <div className="font-yellowtail text-sky-500 font-bold text-center">
       <p className="sm:text-5xl text-4xl">Welcome to your</p>
-      <span className="sm:text-8xl text-7xl">Task World!</span>
+      <span className="sm:text-8xl text-7xl">Task World</span>
     </div>
   );
 };
